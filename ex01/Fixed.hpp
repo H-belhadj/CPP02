dc0a62a0c1d841cd463af15924c6422a61d9af3a
belhadj haitam <steankir.haitam@gmail.com>
2024-01-22 19:33:17 +0100

    1337

--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -6,25 +6,24 @@
 /*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 19:18:59 by hbelhadj          #+#    #+#             */
-/*   Updated: 2024/01/22 19:22:22 by hbelhadj         ###   ########.fr       */
+/*   Updated: 2024/01/22 19:32:55 by hbelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
-# define FIXED_HPP
+#define FIXED_HPP
 
 #include <iostream>
-#include <cmath>
 
-class Fixed
-{
-    private:
-    
-    public:
-        Fixed(){};
-        ~Fixed(){};
-    
-}
+class Fixed {
+private:
+    int value;
+    static const int bits; // Remove the initializer
 
+public:
+    Fixed();
+    ~Fixed();
+};
 
 #endif
+
