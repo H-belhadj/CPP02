b2a9ef4daee262c5dc30ed1fae6679931c6307f9
belhadj haitam <steankir.haitam@gmail.com>
2024-01-22 19:03:12 +0100

    haitam

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,7 +6,19 @@
 /*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 12:37:16 by hbelhadj          #+#    #+#             */
-/*   Updated: 2024/01/21 12:37:17 by hbelhadj         ###   ########.fr       */
+/*   Updated: 2024/01/22 19:02:58 by hbelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+int Fixed::getRawBits()const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->bits;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    this->bits = raw;
+}
