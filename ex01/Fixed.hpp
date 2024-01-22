119211c8e2008fafee8573b967f258525cf883fd
belhadj haitam <steankir.haitam@gmail.com>
2024-01-22 19:21:43 +0100

    jesus

--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -6,7 +6,22 @@
 /*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 19:18:59 by hbelhadj          #+#    #+#             */
-/*   Updated: 2024/01/22 19:19:00 by hbelhadj         ###   ########.fr       */
+/*   Updated: 2024/01/22 19:21:32 by hbelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+    private:
+    
+    public:
+    
+}
+
+
+#endif
