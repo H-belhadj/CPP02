5a1f65d1f40aeb19aa939c9cf85f7b9da9d2d82f
belhadj haitam <steankir.haitam@gmail.com>
2024-01-25 15:42:32 +0100

    soon

--- ex02/Fixed.hpp
+++ ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 23:53:51 by hbelhadj          #+#    #+#             */
-/*   Updated: 2024/01/25 15:30:52 by hbelhadj         ###   ########.fr       */
+/*   Updated: 2024/01/25 15:41:53 by hbelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,8 +45,12 @@ class Fixed
         Fixed operator-(const Fixed &other) const;
         Fixed operator*(const Fixed &other) const;
         Fixed operator/(const Fixed &other) const;
- 
-
+        //The 4 increment/decrement:
+        Fixed& operator++(Fixed& other);
+        Fixed& operator--(Fixed& other);
+        Fixed operator++(Fixed& other, int);
+        Fixed operator--(Fixed& other, int);
+  
 };
 std::ostream& operator<<(std::ostream& COUT, Fixed const &i);
 
